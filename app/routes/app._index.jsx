1978b8f2715fd97a6dece0c018f008b4f2517fed
PavankumarKokane <pavankokane2013@gmail.com>
2024-12-17 08:50:13 +0530

    first commit

---
+++ app/routes/app._index.jsx
@@ -0,0 +1,58 @@
+import {
+  Page,
+  Layout,
+  Text,
+  Card,
+  Button,
+  BlockStack,
+  Box,
+  List,
+  Link,
+  InlineStack,
+} from "@shopify/polaris";
+import { authenticate } from "../shopify.server";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async ({ request }) => {
+  const { admin } = await authenticate.admin(request);
+  const response = await admin.graphql(
+    `#graphql
+    query {
+      products(first: 10) {
+        edges {
+          node {
+            id,
+            title
+          }
+        }
+      }
+    }`,
+  );
+
+  const data = await response.json();
+  return data.data.products.edges;
+};
+
+export default function Index() {
+  const data = useLoaderData();
+  return (
+    <Page>
+      <BlockStack gap="500">
+        <Layout>
+          <Layout.Section>
+            <div style={{ display: "grid", gap: "1rem" }}>
+              {data.map((item, index) => {
+                const productId = item.node.id.split("/").pop();
+                return (
+                  <Card key={index}>
+                    <Link url={`/product/${productId}`}>{item.node.title}</Link>
+                  </Card>
+                );
+              })}
+            </div>
+          </Layout.Section>
+        </Layout>
+      </BlockStack>
+    </Page>
+  );
+}
---
+++ app/routes/orders.jsx
@@ -0,0 +1,66 @@
+import {
+  Page,
+  Layout,
+  Text,
+  Card,
+  Button,
+  BlockStack,
+  Box,
+  List,
+  Link,
+  InlineStack,
+} from "@shopify/polaris";
+import { authenticate } from "../shopify.server";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async ({ request }) => {
+  const { admin } = await authenticate.admin(request);
+  const response = await admin.graphql(
+    `#graphql
+    query {
+        orders(first: 10) {
+            nodes {
+                name
+                email
+                phone
+                totalPrice
+                customer {
+                    addresses {
+                        address1
+                        address2
+                        city
+                        name
+                    }
+                }
+            }
+        }
+    }`,
+  );
+
+  const data = await response.json();
+  return data.data.orders.nodes || null;
+};
+
+export default function Orders() {
+  const data = useLoaderData();
+  return (
+    <Page>
+      <BlockStack gap="500">
+        <Layout>
+          <Layout.Section>
+            <div style={{ display: "grid", gap: "1rem" }}>
+              {data.map((item, index) => {
+                return (
+                  <Card key={index}>
+                    <Text>{item.name} - {item.customer.addresses[0].name}</Text>
+                    <Text>Address :- {item.customer.addresses[0].city}</Text>
+                  </Card>
+                );
+              })}
+            </div>
+          </Layout.Section>
+        </Layout>
+      </BlockStack>
+    </Page>
+  );
+}
