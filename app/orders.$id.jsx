b42040d20e22ec6e68d8ddcd3bae1a45bc4cc9fa
PavankumarAmura <pavankumar@amuratech.com>
2024-12-18 22:59:19 +0530

    orders code

--- app/orders.$id.jsx
+++ app/orders.$id.jsx
@@ -1,35 +1,16 @@
-import {
-  Page,
-  Layout,
-  Text,
-  Card,
-  BlockStack
-} from "@shopify/polaris";
 import { useLoaderData } from "@remix-run/react";
 
-export const loader = async ({ params, request }) => {
-  const order_id = params.id;
-  return order_id;
+export const loader = async ({ params }) => {
+    const { order_id } = params;
+    return order_id;
 };
 
-export default function OrderDetail() {
-  const order_id = useLoaderData();
+export default function ProductPage() {
+    const order_id = useLoaderData();
 
-  if (!order_id) {
-    return <SkeletonPage title="Loading..." />;
-  }
-
-  return (
-    <Page>
-      <BlockStack gap="500">
-        <Layout>
-          <Layout.Section>
-            <Card>
-            {order_id}
-            </Card>
-          </Layout.Section>
-        </Layout>
-      </BlockStack>
-    </Page>
-  );
+    return (
+        <div>
+            <h1>Order ID:- {order_id}</h1>
+        </div>
+    );
 }
