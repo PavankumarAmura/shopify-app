1366f8dfe88340d3672916940663c3eaf7f13c66
PavankumarAmura <pavankumar@amuratech.com>
2024-12-18 23:08:47 +0530

    orders code

--- app/routes/orders.$id.jsx
+++ app/routes/orders.$id.jsx
@@ -9,7 +9,7 @@ import { authenticate } from "../shopify.server";
 import { useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ params, request }) => {
-  const { admin } = await authenticate.admin(request);
+  //const { admin } = await authenticate.admin(request);
   const order_id = params.id;
   return order_id;
 };
