39397f6ada1d6b6184f3a8607ffd8ab71a5e1751
PavankumarAmura <pavankumar@amuratech.com>
2024-12-18 22:55:10 +0530

    orders code

--- app/orders.$id.jsx
+++ app/orders.$id.jsx
@@ -5,11 +5,9 @@ import {
   Card,
   BlockStack
 } from "@shopify/polaris";
-import { authenticate } from "./shopify.server";
 import { useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ params, request }) => {
-  const { admin } = await authenticate.admin(request);
   const order_id = params.id;
   return order_id;
 };
